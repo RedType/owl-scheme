259cb4dbe4150f80ffc69c3899dd95880638864d
Will Young <wil.wyo@gmail.com>
2023-09-05 21:58:26 -0400

    Optimize lookup table

--- src/data.rs
+++ src/data.rs
@@ -1,48 +1,32 @@
 use std::{
+  collections::HashMap,
   fmt,
   rc::Rc,
 };
-use bimap::BiMap;
-
-#[derive(Debug, Default)]
-pub struct IdTable {
-  table: BiMap<String, usize>, 
-  next_id: usize,
-}
-
-impl IdTable {
-  pub fn new() -> Self {
-    Self {
-      table: BiMap::new(),
-      next_id: 0,
-    }
-  }
-
-  pub fn get(&mut self, name: &str) -> usize {
-    if let Some(id) = self.table.get_by_left(name) {
-      *id
-    } else {
-      let id = self.next_id;
-      self.next_id += 1;
-      self.table.insert(name.to_owned(), id);
-
-      id
-    }
-  }
-
-  pub fn lookup(&self, id: usize) -> Option<&str> {
-    self.table.get_by_right(&id).map(|x| x.as_str())
-  }
-}
-
 #[derive(Debug)]
 pub enum Data {
   List(Vec<Rc<Data>>),
-  Symbol(usize),
+  Symbol(usize, Rc<str>),
   String(String),
   Boolean(bool),
   Integer(i64),
   Float(f64),
+}
+
+impl PartialEq for Data {
+  fn eq(&self, other: &Data) -> bool {
+    use Data::*;
+
+    match (self, other) {
+      (List(xs), List(ys)) => xs == ys,
+      (Symbol(x, _), Symbol(y, _)) => x == y,
+      (String(x), String(y)) => x == y,
+      (Boolean(x), Boolean(y)) => x == y,
+      (Integer(x), Integer(y)) => x == y,
+      (Float(x), Float(y)) => x == y, // you should know better
+      _ => false,
+    }
+  }
 }
 
 impl fmt::Display for Data {
@@ -59,9 +43,7 @@ impl fmt::Display for Data {
         }
         write!(f, ")")
       },
-      Symbol(_) => {
-        todo!();
-      },
+      Symbol(_, name) => write!(f, "{}", name),
       String(x) => write!(f, "\"{}\"", x),
       Boolean(x) => write!(f, "{}", if *x { "#true" } else { "#false" }),
       Integer(x) => write!(f, "{}", x),
@@ -70,35 +52,150 @@ impl fmt::Display for Data {
   }
 }
 
+#[derive(Debug, Default)]
+pub struct IdTable {
+  table: HashMap<Rc<str>, usize>,
+  lookup: Vec<Rc<str>>, 
+  next_id: usize,
+}
+
+impl IdTable {
+  pub fn new() -> Self {
+    Self {
+      table: HashMap::new(),
+      lookup: Vec::new(),
+      next_id: 0,
+    }
+  }
+
+  pub fn add(&mut self, name: &str) -> Data {
+    if let Some(&id) = self.table.get(name) {
+      Data::Symbol(id, Rc::clone(&self.lookup[id]))
+    } else {
+      let id = self.next_id;
+      self.next_id += 1;
+      let own_name: Rc<str> = Rc::from(name);
+      self.table.insert(Rc::clone(&own_name), id);
+      self.lookup.push(Rc::clone(&own_name));
+
+      Data::Symbol(id, own_name)
+    }
+  }
+
+  pub fn get(&self, name: &str) -> Option<usize> {
+    self.table.get(name).copied()
+  }
+
+  pub fn get_sym(&self, name: &str) -> Option<Data> {
+    self.table.get(name).map(|&id| Data::Symbol(id, Rc::clone(&self.lookup[id])))
+  }
+
+  pub fn lookup(&self, id: usize) -> Option<Rc<str>> {
+    self.lookup.get(id).map(Rc::clone)
+  }
+
+  pub fn lookup_sym(&self, id: usize) -> Option<Data> {
+    self.lookup.get(id).map(|name| Data::Symbol(id, Rc::clone(name)))
+  }
+}
+
+
 #[cfg(test)]
 mod tests {
   use super::*;
 
+  macro_rules! unsym {
+    ($sym:expr) => {
+      if let Data::Symbol(id, ref name) = $sym {
+        (id, name)
+      } else {
+        panic!("{:?} must be a Data::Symbol", $sym);
+      }
+    }
+  }
+
+
   #[test]
-  fn table_insert() {
+  fn table_add() {
     let mut table = IdTable::new();
 
-    let asdf = table.get("asdf");
-    assert_eq!(table.get("asdf"), asdf);
-
-    let fdsa = table.get("fdsa");
-    assert_eq!(table.get("fdsa"), fdsa);
+    let asdf_sym = table.add("asdf");
+    let fdsa_sym = table.add("fdsa");
+    let asdf_sym_2 = table.add("asdf");
+    let asdf = unsym!(asdf_sym);
+    let fdsa = unsym!(fdsa_sym);
+    let asdf2 = unsym!(asdf_sym_2);
 
     assert_ne!(asdf, fdsa);
+    assert_eq!(asdf, asdf2);
+
+    assert_ne!(asdf.0, fdsa.0);
+    assert_eq!(asdf.0, asdf2.0);
+
+    assert_eq!(asdf.1, &Rc::from("asdf"));
+    assert_eq!(fdsa.1, &Rc::from("fdsa"));
+    assert_eq!(asdf2.1, &Rc::from("asdf"));
+  }
+
+  #[test]
+  fn table_get() {
+    let mut table = IdTable::new();
+
+    assert_eq!(table.get("asdf"), None);
+    assert_eq!(table.get("fdsa"), None);
+
+    let (asdf, _) = unsym!(table.add("asdf"));
+    let (fdsa, _) = unsym!(table.add("fdsa"));
+
+    assert_eq!(table.get("asdf"), Some(asdf));
+    assert_eq!(table.get("fdsa"), Some(fdsa));
+    assert_ne!(table.get("asdf"), table.get("fdsa"));
+  }
+
+  #[test]
+  fn table_get_sym() {
+    let mut table = IdTable::new();
+
+    assert_eq!(table.get_sym("asdf"), None);
+
+    let asdf = table.add("asdf");
+
+    assert_eq!(table.get_sym("asdf").unwrap(), asdf);
   }
 
   #[test]
   fn table_lookup() {
     let mut table = IdTable::new();
 
-    assert_eq!(table.lookup(0), None);
     assert_eq!(table.lookup(678), None);
+    assert_eq!(table.lookup(0), None);
+    assert_eq!(table.lookup(usize::MAX), None);
 
-    let asdf = table.get("asdf");
-    let fdsa = table.get("fdsa");
+    let asdf_sym = table.add("asdf");
+    let fdsa_sym = table.add("fdsa");
+    let asdf = unsym!(asdf_sym).0;
+    let fdsa = unsym!(fdsa_sym).0;
 
-    assert_eq!(table.lookup(asdf), Some("asdf"));
-    assert_eq!(table.lookup(fdsa), Some("fdsa"));
+    assert_eq!(table.lookup(asdf), Some(Rc::from("asdf")));
+    assert_eq!(table.lookup(fdsa), Some(Rc::from("fdsa")));
+
+    assert_eq!(table.lookup(asdf.saturating_add(fdsa).saturating_add(1)), None);
+  }
+
+  #[test]
+  fn table_lookup_sym() {
+    let mut table = IdTable::new();
+
+    assert_eq!(table.lookup_sym(1234), None);
+    assert_eq!(table.lookup_sym(0), None);
+    assert_eq!(table.lookup_sym(usize::MAX), None);
+
+    let asdf = table.add("asdf");
+    let (id, _) = unsym!(asdf);
+
+    assert_eq!(table.lookup_sym(id), Some(asdf));
+
+    assert_eq!(table.lookup_sym(id.saturating_add(1)), None);
   }
 }
 
--- src/parsing/parser.rs
+++ src/parsing/parser.rs
@@ -1,5 +1,4 @@
 use std::{
-  cell::RefCell,
   iter::Peekable,
   rc::Rc,
 };
@@ -13,14 +12,14 @@ use crate::{
 };
 
 fn parse_list<I: Iterator<Item = LexItem>>(
-  lexemes: Rc<RefCell<Peekable<I>>>,
-  identifiers: Rc<RefCell<IdTable>>,
+  lexemes: &mut Peekable<I>,
+  identifiers: &mut IdTable,
   head_info: Info,
 ) -> Result<Data, ParseError> {
   let mut list: Vec<Rc<Data>> = Vec::new();
 
   loop {
-    match lexemes.borrow_mut().peek() {
+    match lexemes.peek() {
       None => {
         return Err(ParseError(ParseErrorKind::MismatchedLParen, head_info));
       },
@@ -28,7 +27,7 @@ fn parse_list<I: Iterator<Item = LexItem>>(
         return Ok(Data::List(list));
       },
       _ => {
-        let next_data = parse_rec(Rc::clone(&lexemes), Rc::clone(&identifiers))?;
+        let next_data = parse_rec(lexemes, identifiers)?;
         list.push(Rc::new(next_data));
       },
     }
@@ -36,25 +35,25 @@ fn parse_list<I: Iterator<Item = LexItem>>(
 }
 
 fn parse_rec<I: Iterator<Item = LexItem>>(
-  lexemes: Rc<RefCell<Peekable<I>>>,
-  identifiers: Rc<RefCell<IdTable>>,
+  lexemes: &mut Peekable<I>,
+  identifiers: &mut IdTable,
 ) -> Result<Data, ParseError> {
-  let data = match lexemes.borrow_mut().next() {
-    Some(LexItem(Lexeme::Identifier(x), _)) => Data::Symbol(identifiers.borrow_mut().get(&x)),
+  let data = match lexemes.next() {
+    Some(LexItem(Lexeme::Identifier(x), _)) => identifiers.add(&x),
     Some(LexItem(Lexeme::Boolean(x), _)) => Data::Boolean(x),
     Some(LexItem(Lexeme::String(x), _)) => Data::String(x),
     Some(LexItem(Lexeme::Integer(x), _)) => Data::Integer(x),
     Some(LexItem(Lexeme::Float(x), _)) => Data::Float(x),
 
     Some(LexItem(Lexeme::LParen, info)) =>
-      parse_list(Rc::clone(&lexemes), Rc::clone(&identifiers), info)?,
+      parse_list(lexemes, identifiers, info)?,
     Some(LexItem(Lexeme::RParen, info)) => {
       return Err(ParseError(ParseErrorKind::MismatchedRParen, info));
     },
     Some(LexItem(Lexeme::Quote, _)) => {
       let list = vec![
-        Rc::new(Data::Symbol(identifiers.borrow_mut().get("quote"))),
-        Rc::new(parse_rec(Rc::clone(&lexemes), Rc::clone(&identifiers))?),
+        Rc::new(identifiers.add("quote")),
+        Rc::new(parse_rec(lexemes, identifiers)?),
       ];
       Data::List(list)
     },
@@ -69,15 +68,15 @@ pub fn parse<I>(sequence: I) -> Result<(Vec<Data>, IdTable), ParseError>
 where
   I: IntoIterator<Item = LexItem>,
 {
-  let lexemes = Rc::new(RefCell::new(sequence.into_iter().peekable()));
-  let identifiers = Rc::new(RefCell::new(IdTable::new()));
+  let mut lexemes = sequence.into_iter().peekable();
+  let mut identifiers = IdTable::new();
   let mut data = Vec::new();
 
-  while let Some(_) = lexemes.borrow_mut().peek() {
-    data.push(parse_rec(Rc::clone(&lexemes), Rc::clone(&identifiers))?);
+  while let Some(_) = lexemes.peek() {
+    data.push(parse_rec(&mut lexemes, &mut identifiers)?);
   }
 
-  Ok((data, Rc::try_unwrap(identifiers).unwrap().take()))
+  Ok((data, identifiers))
 }
 
 #[cfg(test)]
@@ -93,7 +92,6 @@ mod tests {
   #[test]
   fn parse_empty() {
     let result = parse([]);
-    assert!(result.is_ok());
     assert!(result.unwrap().0.is_empty());
   }
 }
