82f34bb6f0b86fa2c6d58cf958258d170c21b54e
Will Young <wil.wyo@gmail.com>
2023-11-07 22:52:39 -0500

    Move Data stringification into VM

--- src/data.rs
+++ src/data.rs
@@ -1,11 +1,19 @@
 use std::{
-  collections::{HashMap, HashSet, VecDeque},
+  collections::{HashSet, VecDeque},
   fmt,
   rc::Rc,
 };
 use gc::{Finalize, GcCell, Trace};
 
-pub type BuiltinFn = FnMut(&mut Data) -> Result<Data, ()>;
+// trait alias
+pub trait BuiltinFn: FnMut(&mut Data) -> Result<Data, ()> {}
+impl<T: FnMut(&mut Data) -> Result<Data, ()>> BuiltinFn for T {}
+
+impl fmt::Debug for dyn BuiltinFn {
+  fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+    write!(f, "<builtin fn>")
+  }
+}
 
 #[derive(Debug, Finalize, Trace)]
 pub enum Data {
@@ -14,7 +22,7 @@ pub enum Data {
   String(String),
   Boolean(bool),
   // first element is the fn name
-  Builtin(Rc<str>, Rc<dyn BuiltinFn>),
+  Builtin(Rc<str>, #[unsafe_ignore_trace] Rc<dyn BuiltinFn>),
   // numbers
   Complex(f64, f64),
   Real(f64),
@@ -28,7 +36,7 @@ impl Data {
   }
 
   pub fn is_nil(&self) -> bool {
-    if let Self::List(xs) {
+    if let Self::List(xs) = self {
       xs.is_empty()
     } else {
       false
@@ -36,60 +44,32 @@ impl Data {
   }
 }
 
-impl fmt::Display for Data {
-  fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+impl PartialEq for Data {
+  fn eq(&self, other: &Self) -> bool {
     use Data::*;
-
-    match self {
-      List(xs) => {
-        let mut iter = xs.iter().peekable();
-        let mut first = true;
-
-        write!(f, "(")?;
-        while let Some(x) = iter.next() {
-          let last = iter.peek().is_none();
-          // write leading space
-          if first { first = false; } else if !last { write!(f, " ")?; }
-
-          // check to see if we're at the end
-          // if so, print a dot for non-nil and print nothing for nil
-          // otherwise just print the element
-          if !last {
-            write!(f, "{}", x.borrow())?;
-          } else if !x.borrow().is_nil() {
-            write!(f, " . {}", x.borrow())?;
-          }
-        }
-        write!(f, ")")
-      },
-      Symbol(name) => write!(f, "{}", name),
-      String(x) => write!(f, "\"{}\"", x),
-      Boolean(x) => write!(f, "{}", if *x { "#true" } else { "#false" }),
-      Complex(r, i) => {
-        if i == 0.0 {
-          write!(f, "{}", r)
-        } else if r == 0.0 {
-          write!(f, "{}i", i)
+    match (self, other) {
+      (List(l), List(r)) => {
+        if l.len() != r.len() {
+          false
         } else {
-          let sign = if i < 0 { "-" } else { "+" };
-          write!(f, "{}{}{}i", r, sign, i)
+          l.iter().zip(r.iter())
+            .map(|(l, r)| *l.borrow() == *r.borrow())
+            .fold(true, |a, x| a && x)
         }
       },
-      Real(x) => write!(f, "{}", x),
-      Rational(n, d) => {
-        if d == 1 {
-          write!(f, "{}", n)
-        } else if n == 0 {
-          write!(f, "0")
-        } else {
-          write!(f, "{}/{}", n, d)
-        }
-      },
-      Integer(x) => write!(f, "{}", x),
-      Builtin(name, _) => write!(f, "<builtin fn {}>", name),
+      (Symbol(l), Symbol(r)) => Rc::ptr_eq(l, r),
+      (String(l), String(r)) => l == r,
+      (Boolean(l), Boolean(r)) => l == r,
+      (Builtin(_, l), Builtin(_, r)) => Rc::ptr_eq(l, r),
+      (Complex(lr, li), Complex(rr, ri)) => lr == rr && li == ri,
+      (Real(l), Real(r)) => l == r,
+      (Rational(_, _), Rational(_, _)) => unimplemented!("Rationals are unreduced"),
+      (Integer(l), Integer(r)) => l == r,
+      _ => false,
     }
   }
 }
+impl Eq for Data {}
 
 #[derive(Debug, Default)]
 pub struct SymbolTable(HashSet<Rc<str>>);
@@ -119,48 +99,6 @@ impl SymbolTable {
 mod tests {
   use super::*;
 
-  #[test]
-  fn display() {
-    use Data::*;
-
-    let mut table = SymbolTable::new();
-    let symbol = table.add("jeremy");
-    assert_eq!(symbol.to_string(), "jeremy");
-
-    let string = String("upright bass".to_owned());
-    assert_eq!(string.to_string(), "\"upright bass\"");
-
-    let tru = Boolean(true);
-    let fals = Boolean(false);
-    assert_eq!(tru.to_string(), "#true");
-    assert_eq!(fals.to_string(), "#false");
-
-    let int = Integer(35);
-    assert_eq!(int.to_string(), "35");
-
-    let float = Real(123.4);
-    assert_eq!(float.to_string(), "123.4");
-
-    let nil = Data::nil();
-    assert_eq!(nil.to_string(), "()");
-
-    let list = List(
-      [symbol, string, tru, fals, int, float, nil]
-        .into_iter()
-        .map(GcCell::new)
-        .collect::<VecDeque<_>>()
-    );
-    assert_eq!(list.to_string(), "(jeremy \"upright bass\" #true #false 35 123.4)");
-
-    let dotted = List(
-      [Real(12.0), String("oy".into()), Data::nil(), Real(0.23456)]
-        .into_iter()
-        .map(GcCell::new)
-        .collect::<VecDeque<_>>()
-    );
-    assert_eq!(dotted.to_string(), "(12 \"oy\" () . 0.23456)")
-  }
-
   macro_rules! unsym {
     ($sym:expr) => {
       if let Data::Symbol(ref name) = $sym {
@@ -169,30 +107,6 @@ mod tests {
         panic!("{:?} must be a Data::Symbol", $sym);
       }
     }
-  }
-
-  #[test]
-  fn display_numbers() {
-    let complex = Complex(123.4, 5.0);
-    assert_eq!(complex.to_string(), "123.4+5.0i");
-
-    let complex2 = Complex(123.4, -5.0);
-    assert_eq!(complex2.to_string(), "123.4-5.0i");
-
-    let complex3 = Complex(0.0, -5.0);
-    assert_eq!(complex3.to_string(), "-5.0i");
-
-    let complex4 = Complex(1.1, 0.0);
-    assert_eq!(complex4.to_string(), "1.1");
-
-    let rational = Data::rat(3, -4);
-    assert_eq!(rational.to_string(), "-3/4");
-
-    let rational2 = Rational(0, 4);
-    assert_eq!(rational2.to_string(), "0");
-
-    let rational3 = Rational(4, 1);
-    assert_eq!(rational3.to_string(), "4");
   }
 
   #[test]
