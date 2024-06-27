2c482dffc56e9c87ced524140c9a5c94c7cfe5e9
Will Young <wil.wyo@gmail.com>
2024-06-26 22:38:28 -0400

    Refactor standard library implementations

--- src/stdlib/mod.rs
+++ src/stdlib/mod.rs
@@ -1,212 +1,206 @@
-use std::iter;
 use crate::{
   data::Data,
+  error::UnspecifiedError,
   vm::VM,
 };
-
-macro_rules! builtin {
-  ($vm:ident, $name:expr, $f:expr) => {
-    $vm.def_builtin($name, |data| if let List(xs) = data {
-      // fetch iterator and effective length of argument list (without trailing nil)
-      let (mut iter, len) = if xs.back().unwrap().borrow().is_nil() {
-        (xs.iter().take(xs.len() - 1), xs.len() - 1)
-      } else {
-        (xs.iter().take(xs.len()), xs.len())
-      };
-      $f($vm, iter, len)
-    } else if data.is_nil() {
-      $f($vm, iter::empty(), 0)
-    } else {
-      panic!("Failed precondition: builtins must be passed an argument list")
-    })
-  };
-}
 
 pub fn import_std(vm: &mut VM) {
   use Data::*;
 
-  builtin!(vm, "number?", |_, args, _| {
-    Ok(Boolean(args.fold(true, |a, x| a && match x {
+  vm.def_builtin("number?", |args| {
+    let res = args.iter().fold(true, |a, x| a && match *x.borrow() {
       Complex(_, _) | Real(_) | Rational(_, _) | Integer(_) => true,
       _ => false,
-    })))
+    });
+    Ok(Boolean(res))
   });
 
-  builtin!(vm, "complex?", |_, args, _| {
-    Ok(Boolean(args.fold(true, |a, x| a && match x {
+  vm.def_builtin("complex?", |args| {
+    let res = args.iter().fold(true, |a, x| a && match *x.borrow() {
       Complex(_, _) | Real(_) | Rational(_, _) | Integer(_) => true,
       _ => false,
-    })))
+    });
+    Ok(Boolean(res))
   });
 
-  builtin!(vm, "real?", |_, args, _| {
-    Ok(Boolean(args.fold(true, |a, x| a && match x {
+  vm.def_builtin("real?", |args| {
+    let res = args.iter().fold(true, |a, x| a && match *x.borrow() {
       Complex(_, i) => i == 0.0,
       Real(_) | Rational(_, _) | Integer(_) => true,
       _ => false,
-    })))
+    });
+    Ok(Boolean(res))
   });
 
-  builtin!(vm, "rational?", |_, args, _| {
-    Ok(Boolean(args.fold(true, |a, x| a && match x {
+  vm.def_builtin("rational?", |args| {
+    let res = args.iter().fold(true, |a, x| a && match *x.borrow() {
       Complex(r, i) => i == 0.0 && r.round() == r,
       Real(n) => n.round() == n,
       Rational(_, _) | Integer(_) => true,
       _ => false,
-    })))
+    });
+    Ok(Boolean(res))
   });
 
-  builtin!(vm, "integer?", |_, args, _| {
-    Ok(Boolean(args.fold(true, |a, x| a && match x {
+  vm.def_builtin("integer?", |args| {
+    let res = args.iter().fold(true, |a, x| a && match *x.borrow() {
       Complex(r, i) => i == 0.0 && r.round() == r,
       Real(n) => n.round() == n,
       Rational(_, d) => d == 1,
       Integer(_) => true,
       _ => false,
-    })))
+    });
+    Ok(Boolean(res))
   });
 
-  builtin!(vm, "exact?", |_, args, _| {
-    Ok(Boolean(args.fold(true, |a, x| a && match x {
+  vm.def_builtin("exact?", |args| {
+    let res = args.iter().fold(true, |a, x| a && match *x.borrow() {
       Rational(_, _) | Integer(_) => true,
       _ => false,
-    })))
+    });
+    Ok(Boolean(res))
   });
 
-  builtin!(vm, "inexact?", |_, args, _| {
-    Ok(Boolean(args.fold(true, |a, x| a && match x {
+  vm.def_builtin("inexact?", |args| {
+    let res = args.iter().fold(true, |a, x| a && match *x.borrow() {
       Complex(_, _) | Real(_) => true,
       _ => false,
-    })))
+    });
+    Ok(Boolean(res))
   });
 
-  builtin!(vm, "exact-integer?", |_, args, _| {
-    Ok(Boolean(args.fold(true, |a, x| a && match x {
+  vm.def_builtin("exact-integer?", |args| {
+    let res = args.iter().fold(true, |a, x| a && match *x.borrow() {
       Rational(_, d) => d == 1,
       Integer(_) => true,
       _ => false,
-    })))
+    });
+    Ok(Boolean(res))
   });
 
-  builtin!(vm, "finite?", |_, args, _| {
-    Ok(Boolean(args.fold(true, |a, x| a && match x {
+  vm.def_builtin("finite?", |args| {
+    let res = args.iter().fold(true, |a, x| a && match *x.borrow() {
       Complex(r, i) => r.is_finite() && i.is_finite(),
       Real(n) => n.is_finite(),
       Rational(_, _) | Integer(_) => true,
       _ => false,
-    })))
+    });
+    Ok(Boolean(res))
   });
 
-  builtin!(vm, "infinite?", |_, args, _| {
-    Ok(Boolean(args.fold(true, |a, x| a && match x {
+  vm.def_builtin("infinite?", |args| {
+    let res = args.iter().fold(true, |a, x| a && match *x.borrow() {
       Complex(r, i) => r.is_infinite() || i.is_infinite(),
       Real(n) => n.is_infinite(),
       _ => false,
-    })))
+    });
+    Ok(Boolean(res))
   });
 
-  builtin!(vm, "+", |vm, xs, _| {
-    let res = xs.fold(Integer(0), |a, x| match (a, x.borrow()) {
-      (Integer(l), Integer(r)) =>
+  vm.def_builtin("+", |xs| {
+    let res = xs.iter().fold(Integer(0), |a, x| match (a, &*x.borrow()) {
+      (Integer(l), &Integer(r)) =>
         Integer(l + r),
-      (Integer(i), Rational(n, d)) | (Rational(n, d), Integer(i)) =>
-        Rational(i * d + n, d),
-      (Integer(i), Real(r)) | (Real(r), Integer(i)) =>
+      (Integer(i), &Rational(n, d)) | (Rational(n, d), &Integer(i)) =>
+        Rational(i * d as i64 + n, d),
+      (Integer(i), &Real(r)) | (Real(r), &Integer(i)) =>
         Real(r + i as f64),
-      (Integer(n), Complex(r, i)) | (Complex(r, i), Integer(n)) =>
-        Complex(r + n, i),
-      (Rational(ln, ld), Rational(rn, rd)) => {
+      (Integer(n), &Complex(r, i)) | (Complex(r, i), &Integer(n)) =>
+        Complex(r + n as f64, i),
+      (Rational(ln, ld), &Rational(rn, rd)) => {
         let n = ln * rd as i64 + rn * ld as i64;
         let d = ld * rd;
-        vm.rat(n, d as i64)
+        Rational(n, d)
       },
-      (Rational(n, d), Real(r)) | (Real(r), Rational(n, d)) =>
-        Rational(r * d + n, d),
-      (Rational(n, d), Complex(r, i)) | (Complex(r, i), Rational(n, d)) =>
+      (Rational(n, d), &Real(r)) | (Real(r), &Rational(n, d)) =>
+        Real((n as f64 / d as f64) + r),
+      (Rational(n, d), &Complex(r, i)) | (Complex(r, i), &Rational(n, d)) =>
         Complex(n as f64 / d as f64 + r, i),
-      (Real(l), Real(r)) =>
+      (Real(l), &Real(r)) =>
         Real(l + r),
-      (Real(n), Complex(r, i)) | (Complex(r, i), Real(n)) =>
+      (Real(n), &Complex(r, i)) | (Complex(r, i), &Real(n)) =>
         Complex(n + r, i),
-      (Complex(lr, li), Complex(rr, ri)) =>
+      (Complex(lr, li), &Complex(rr, ri)) =>
         Complex(lr + rr, li + ri),
-        
+
       _ => Data::nil(),
     });
-    if res.is_nil() { Err(()) } else { Ok(res) }
+    if res.is_nil() { Err(Box::new(UnspecifiedError)) } else { Ok(res) }
   });
 
-  builtin!(vm, "*", |xs, _| {
-    let res = xs.fold(Integer(1), |a, x| match (a, x.borrow()) {
-      (Integer(l), Integer(r)) =>
+  vm.def_builtin("*", |xs| {
+    let res = xs.iter().fold(Integer(1), |a, x| match (a, &*x.borrow()) {
+      (Integer(l), &Integer(r)) =>
         Integer(l * r),
-      (Integer(i), Rational(n, d)) | (Rational(n, d), Integer(i)) =>
+      (Integer(i), &Rational(n, d)) | (Rational(n, d), &Integer(i)) =>
         Rational(i * n, d),
-      (Integer(i), Real(r)) | (Real(r), Integer(i)) =>
+      (Integer(i), &Real(r)) | (Real(r), &Integer(i)) =>
         Real(r * i as f64),
-      (Integer(n), Complex(r, i)) | (Complex(r, i), Integer(n)) =>
-        Complex(r as f64 * n, r as f64 * i),
-      (Rational(ln, ld), Rational(rn, rd)) =>
-        vm.rat(ln * rn, (ld * rd) as i64),
-      (Rational(n, d), Real(r)) | (Real(r), Rational(n, d)) =>
-        Rational(r * n, d),
-      (Rational(n, d), Complex(r, i)) | (Complex(r, i), Rational(n, d)) => {
+      (Integer(n), &Complex(r, i)) | (Complex(r, i), &Integer(n)) =>
+        Complex(r * n as f64, i * n as f64),
+      (Rational(ln, ld), &Rational(rn, rd)) =>
+        Rational(ln * rn, ld * rd),
+      (Rational(n, d), &Real(r)) | (Real(r), &Rational(n, d)) =>
+        Real((n as f64 / d as f64) * r),
+      (Rational(n, d), &Complex(r, i)) | (Complex(r, i), &Rational(n, d)) => {
         let rat = n as f64 / d as f64;
         Complex(rat * r, rat * i)
       },
-      (Real(l), Real(r)) =>
+      (Real(l), &Real(r)) =>
         Real(l * r),
-      (Real(n), Complex(r, i)) | (Complex(r, i), Real(n)) =>
+      (Real(n), &Complex(r, i)) | (Complex(r, i), &Real(n)) =>
         Complex(n * r, n * i),
-      (Complex(lr, li), Complex(rr, ri)) =>
+      (Complex(lr, li), &Complex(rr, ri)) =>
         Complex(lr * rr - li * ri, lr * ri + li * rr),
-        
+
       _ => Data::nil(),
     });
-    if res.is_nil() { Err(()) } else { Ok(res) }
+    if res.is_nil() { Err(Box::new(UnspecifiedError)) } else { Ok(res) }
   });
 
-  builtin!(vm, "-", |xs, len| {
-    if len == 0 {
-      Err(())
-    } else if len == 1 {
-      return match xs.next() {
-        Integer(x) => Ok(Integer(-x)),
-        Real(x) => Ok(Real(-x)),
-        _ => Err(()),
+  vm.def_builtin("-", |xs| {
+    if xs.len() == 0 {
+      Err(Box::new(UnspecifiedError))
+    } else if xs.len() == 1 {
+      return match &*xs[0].borrow() {
+        &Integer(x) => Ok(Integer(-x)),
+        &Real(x) => Ok(Real(-x)),
+        _ => Err(Box::new(UnspecifiedError)),
       };
     } else {
-      let minuend = xs.next();
-      let res = xs.fold(minuend.borrow(), |a, x| match (a, x.borrow()) {
-        (Integer(l), Integer(r)) => Integer(l - r),
-        (Real(l), Integer(r)) => Real(l - r as f64),
-        (Integer(l), Real(r)) => Real(l as f64 - r),
-        (Real(l), Real(r)) => Real(l - r),
+      let mut xs_iter = xs.iter();
+      let minuend = xs_iter.next().unwrap().borrow().clone_numeric()?;
+      let res = xs_iter.fold(minuend, |a, x| match (a, &*x.borrow()) {
+        (Integer(l), &Integer(r)) => Integer(l - r),
+        (Real(l), &Integer(r)) => Real(l - r as f64),
+        (Integer(l), &Real(r)) => Real(l as f64 - r),
+        (Real(l), &Real(r)) => Real(l - r),
         _ => Data::nil(),
       });
-      if res.is_nil() { Err(()) } else { Ok(res) }
+      if res.is_nil() { Err(Box::new(UnspecifiedError)) } else { Ok(res) }
     }
   });
 
-  builtin!(vm, "/", |xs, len| {
-    if len == 0 {
-      Err(())
-    } else if len == 1 {
-      return match xs.next() {
-        Integer(x) => Ok(Real(1 / x as f64)),
-        Real(x) => Ok(Real(1 / x)),
-        _ => Err(()),
+  vm.def_builtin("/", |xs| {
+    if xs.len() == 0 {
+      Err(Box::new(UnspecifiedError))
+    } else if xs.len() == 1 {
+      return match &*xs[0].borrow() {
+        &Integer(x) => Ok(Real(1.0 / x as f64)),
+        &Real(x) => Ok(Real(1.0 / x)),
+        _ => Err(Box::new(UnspecifiedError)),
       };
     } else {
-      let dividend = xs.next();
-      let res = xs.fold(dividend.borrow(), |a, x| match (a, x.borrow()) {
-        (Integer(l), Integer(r)) => Real(l as f64 / r as f64),
-        (Real(l), Integer(r)) => Real(l / r as f64),
-        (Integer(l), Real(r)) => Real(l as f64 / r),
-        (Real(l), Real(r)) => Real(l / r),
+      let mut xs_iter = xs.iter();
+      let dividend = xs_iter.next().unwrap().borrow().clone_numeric()?;
+      let res = xs_iter.fold(dividend, |a, x| match (a, &*x.borrow()) {
+        (Integer(l), &Integer(r)) => Real(l as f64 / r as f64),
+        (Real(l), &Integer(r)) => Real(l / r as f64),
+        (Integer(l), &Real(r)) => Real(l as f64 / r),
+        (Real(l), &Real(r)) => Real(l / r),
         _ => Data::nil(),
-      });
-      if res.is_nil() { Err(()) } else { Ok(res) }
+        });
+      if res.is_nil() { Err(Box::new(UnspecifiedError)) } else { Ok(res) }
     }
   });
 }
